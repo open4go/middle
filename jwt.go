005f935ba03a003a3c9afe0a36f8b61fb3237799
Frank <frank@Franks-iMac.local>
2023-07-13 23:45:12 +0800

    update middle for sign out

--- jwt.go
+++ jwt.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 )
 
+const (
+	SignOutPath = "v1/auth/merchant/signout"
+)
+
 // JWTMiddleware 验证cookie并且将解析出来的账号
 // 通过账号获取角色
 // 通过角色判断其是否具有该api的访问权限
@@ -14,35 +18,47 @@ import (
 // 通过hget api/path/ role boolean
 func JWTMiddleware(key []byte) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		cookie, err := c.Cookie("jwt")
-		if cookie == "" {
-			log.Error("cookie name as jwt no found")
-			c.AbortWithStatus(http.StatusUnauthorized)
-			return
+		reqPath := c.FullPath()
+		if SignOutPath == reqPath {
+			if isLogin(c, key) {
+				c.Next()
+			} else {
+				c.AbortWithStatus(http.StatusAlreadyReported)
+			}
+		} else {
+			if isLogin(c, key) {
+				c.Next()
+			} else {
+				c.AbortWithStatus(http.StatusForbidden)
+			}
 		}
-
-		token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-			return key, nil
-		})
-
-		if err != nil {
-			log.WithField("message", "parse claims failed").Error(err)
-			c.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-
-		claims := token.Claims.(*jwt.StandardClaims)
-		l := &LoginInfo{}
-		err = l.Load(claims.Issuer)
-		if err != nil {
-			log.WithField("message", "LoadLoginInfo failed").Error(err)
-			c.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-		// 写入解析客户的jwt token后得到的数据
-		l.WriteIntoHeader(c)
-		// 检测角色是否有权限
-
-		c.Next()
 	}
+}
+
+func isLogin(c *gin.Context, key []byte) bool {
+	cookie, err := c.Cookie("jwt")
+	if cookie == "" {
+		log.Error("cookie name as jwt no found")
+		return false
+	}
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+
+	if err != nil {
+		log.WithField("message", "parse claims failed").Error(err)
+		return false
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+	l := &LoginInfo{}
+	err = l.Load(claims.Issuer)
+	if err != nil {
+		log.WithField("message", "LoadLoginInfo failed").Error(err)
+		return false
+	}
+	// 写入解析客户的jwt token后得到的数据
+	l.WriteIntoHeader(c)
+	return true
 }
