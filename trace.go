dec48afaec87024f2988fb9d8aab5016e7c25182
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2023-12-30 05:37:01 +0800

    feature: update logs set

--- trace.go
+++ trace.go
@@ -14,7 +14,16 @@ func InitLogger(logLevel string) {
 	// 输出到终端
 	logger.SetOutput(os.Stdout)
 	// Add this line for logging filename and line number!
-	logger.SetReportCaller(true)
+	// 中间件打印文件及函数位置毫无意义
+	// {"account_id":"AC1739403045495640064",
+	// "file":"/Users/frank/code/go-workspace/pkg/mod/github.com/open4go/rest@v0.1.5/render.go:21",
+	// "func":"github.com/open4go/rest.MakeResponse",
+	// "id":"","level":"error","message":"参数错误",
+	// "msg":"Key: 'CreateRequest.Desc' Error:Field validation for 'Desc' failed on the 'required' tag",
+	// "request_id":"2b42540f-9ab9-4a37-8d2d-6ef7c1a52229",
+	// "result":{"name":"a","phone":"1766612","password":"12345678","desc":""},
+	// "time":"2023-12-30T05:28:31+08:00"}
+	//logger.SetReportCaller(true)
 	// 日志格式
 	logger.SetFormatter(&logrus.JSONFormatter{})
 	// 设置日志级别
