f753c6c5afe4187fe3599eebe0eed4e0b82e1eeb
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-05-25 23:19:39 +0800

    update jwt token

--- JWTAuthMiddleware.go
+++ JWTAuthMiddleware.go
@@ -3,23 +3,18 @@ package middle
 import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
+	"github.com/open4go/log"
 	"net/http"
 	"strings"
 )
 
-var jwtSecret = []byte("your_secret_key")
-
-func init() {
-	jwtSecret = []byte(viper.GetString("jwt.key"))
-}
-
 // JWTAuthMiddleware 是一个 Gin 中间件，用于验证 JWT token
 // 用户于客户端app/api验证
-func JWTAuthMiddleware() gin.HandlerFunc {
+func JWTAuthMiddleware(jwtSecret []byte) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
+			log.Log().Error("authorization header is required")
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "authorization header is required"})
 			c.Abort()
 			return
@@ -27,6 +22,8 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 
 		parts := strings.SplitN(authHeader, " ", 2)
 		if len(parts) != 2 || parts[0] != "Bearer" {
+			log.Log().WithField("authHeader", authHeader).
+				Error("authorization header is required")
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "authorization header format must be Bearer {token}"})
 			c.Abort()
 			return
@@ -34,8 +31,11 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 
 		tokenString := parts[1]
 
-		token, claims, err := parseToken(tokenString)
+		token, claims, err := parseToken(tokenString, jwtSecret)
 		if err != nil || !token.Valid {
+			log.Log().WithField("authHeader", authHeader).
+				WithField("token", token).
+				Error("authorization header is required")
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
 			c.Abort()
 			return
@@ -48,7 +48,7 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 }
 
 // 解析 JWT token
-func parseToken(tokenString string) (*jwt.Token, *jwt.StandardClaims, error) {
+func parseToken(tokenString string, jwtSecret []byte) (*jwt.Token, *jwt.StandardClaims, error) {
 	claims := &jwt.StandardClaims{}
 
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
