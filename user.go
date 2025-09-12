8b548ca3797ba0086351ad9b4018907f124f869e
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2025-09-13 01:53:04 +0800

    reverse

--- user.go
+++ user.go
@@ -112,7 +112,8 @@ func (l *LoginInfo) WriteIntoHeader(c *gin.Context) {
 	if tenantId != "" {
 		// 如果存在租户id 则当前是需要传递其作为商户id 并且作为数据隔离
 		c.Request.Header.Set("MerchantID", tenantId)
-
+	} else {
+		// 后续传递默认root 或者 * 标识超级管理员查看所有租户数据
 		// 如果网关没有获取到，那么还需解析看看后台是否是超级super 可以查看所有商户到信息
 		merchantId := c.Request.Header.Get("X-Merchant-ID")
 		// 如果id不为空
@@ -122,13 +123,8 @@ func (l *LoginInfo) WriteIntoHeader(c *gin.Context) {
 			if err != nil {
 				log.Log(c.Request.Context()).Error(err)
 			}
-			tenantId = rs
+			c.Request.Header.Set("MerchantID", rs)
 		}
-
-	} else {
-		tenantId = l.MerchantID
-		// 后续传递默认root 或者 * 标识超级管理员查看所有租户数据
-		c.Request.Header.Set("MerchantID", l.MerchantID)
 	}
 
 	c.Request.Header.Set("AccountID", l.AccountID)
