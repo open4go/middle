67600d24a108b599b5581caaa7ac479398b67804
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2025-09-13 23:43:06 +0800

    fix

--- user.go
+++ user.go
@@ -125,7 +125,7 @@ func (l *LoginInfo) WriteIntoHeader(c *gin.Context) {
 				log.Log(c.Request.Context()).Error(err)
 			}
 			log.Log(c.Request.Context()).WithField("rs", rs).Debug("merchantId is received")
-			c.Request.Header.Set("MerchantID", rs)
+			c.Request.Header.Set("X-Tenant-ID", rs)
 			// 超级管理员需要覆盖这个值
 			if merchantId == "*" {
 				// 代表所有站点到数据
